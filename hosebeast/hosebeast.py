dbf57143cf3c9704ae74decaca00b96e349a5920
Evan Jones <evan_t_jones@mac.com>
2024-08-14 06:52:00 -0500

    Added basic hosebeast page with buttons to control relays

--- hosebeast/hosebeast.py
+++ hosebeast/hosebeast.py
@@ -1,9 +1,14 @@
 """Welcome to Reflex!."""
 
 # Import all the pages.
-from .pages import *
+# Ignore the unused imports here; they have template side effects
+# that add them to the app
+from .pages import index, about, profile, settings, table  # noqa: F401
+from .templates import template
 from . import styles
 
+# from . import relay_control
+from .relay_control import set_relay, RELAY_1, RELAY_2
 import reflex as rx
 
 
@@ -14,3 +19,63 @@ app = rx.App(
     title="Dashboard Template",
     description="A dashboard template for Reflex.",
 )
+
+
+class HosebeastState(rx.State):
+    """The app state."""
+
+    relay_1_on: bool = False
+    relay_2_on: bool = False
+
+    async def toggle_relay_1(self):
+        self.relay_1_on = not self.relay_1_on
+        # print(f"Relay 1 is now {self.relay_1_on}")
+        set_relay(RELAY_1, self.relay_1_on)
+
+    async def toggle_relay_2(self):
+        self.relay_2_on = not self.relay_2_on
+        # print(f"Relay 2 is now {self.relay_2_on}")
+        set_relay(RELAY_2, self.relay_2_on)
+
+
+@template(route="/hosebeast", title="Hosebeast")
+def hosebeast_layout() -> rx.Component:
+    """The main layout of the app."""
+    return rx.vstack(
+        rx.heading("Hosebeast, Eh", size="2xl"),
+        rx.text(
+            "Get started by editing ",
+            rx.code("hosebeast/pages/index.py"),
+            ".",
+        ),
+        rx.cond(
+            HosebeastState.relay_1_on,
+            rx.button(
+                "Turn off Relay 1",
+                on_click=HosebeastState.toggle_relay_1,
+                color_scheme="red",
+                width="100%",
+            ),
+            rx.button(
+                "Turn on Relay 1",
+                on_click=HosebeastState.toggle_relay_1,
+                color_scheme="green",
+                width="100%",
+            ),
+        ),
+        rx.cond(
+            HosebeastState.relay_2_on,
+            rx.button(
+                "Turn off Relay 2",
+                on_click=HosebeastState.toggle_relay_2,
+                color_scheme="red",
+                width="100%",
+            ),
+            rx.button(
+                "Turn on Relay 2",
+                on_click=HosebeastState.toggle_relay_2,
+                color_scheme="green",
+                width="100%",
+            ),
+        ),
+    )
---
+++ hosebeast/relay_control.py
@@ -0,0 +1,117 @@
+#! /usr/bin/env python3
+
+import asyncio
+import time
+
+try:
+    import RPi.GPIO as GPIO
+except Exception:
+    print("Error importing RPi.GPIO. Using Mock.GPIO instead")
+    import Mock.GPIO as GPIO
+
+"""
+Demo Pinout & wiring:
+
+RasPi 3B:
+   3V3  (1) (2)  5V    
+ GPIO2  (3) (4)  5V       <----  (To VCC on Relay)
+ GPIO3  (5) (6)  GND      <----  (To GND on Relay)
+ GPIO4  (7) (8)  GPIO14
+   GND  (9) (10) GPIO15
+GPIO17 (11) (12) GPIO18   <----  (To IN1 on Relay)
+GPIO27 (13) (14) GND   
+GPIO22 (15) (16) GPIO23   <----  (To IN2 on Relay)
+   3V3 (17) (18) GPIO24
+GPIO10 (19) (20) GND   
+ GPIO9 (21) (22) GPIO25
+GPIO11 (23) (24) GPIO8 
+   GND (25) (26) GPIO7 
+ GPIO0 (27) (28) GPIO1 
+ GPIO5 (29) (30) GND   
+ GPIO6 (31) (32) GPIO12
+GPIO13 (33) (34) GND   
+GPIO19 (35) (36) GPIO16
+GPIO26 (37) (38) GPIO20
+   GND (39) (40) GPIO21
+
+We're interested only in the pins 
+in the north-west corner of the board:
+5V, GND, GPIO18, GPIO23
+,--------------------------------.
+| oooooooooooooooooooo J8     +====
+| 1ooooooooooooooooooo        | USB
+|                             +====
+| o1 RUN  Pi Model 3B  V1.2      |
+| |D      +---+               +====
+| |S      |SoC|               | USB
+| |I      +---+               +====
+| |0               C|            |
+|                  S|       +======
+|                  I| |A|   |   Net
+| pwr      |HDMI|  0| |u|   +======
+`-| |------|    |-----|x|--------'
+
+
+
+"""
+
+RELAY_1 = 18
+RELAY_2 = 23
+
+PIN_NAMES = {
+    RELAY_1: "Relay 1",
+    RELAY_2: "Relay 2",
+}
+
+
+def configure_relays():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(RELAY_1, GPIO.OUT)
+    GPIO.setup(RELAY_2, GPIO.OUT)
+
+
+def set_relay(relay_pin: int, state: bool):
+    desc = PIN_NAMES.get(relay_pin)
+    if desc:
+        print(f"{desc} {'ON' if state else 'OFF'} ")
+    GPIO.output(relay_pin, state)
+
+
+async def relay_on(relay_pin: int, duration: float | None = None):
+    desc = PIN_NAMES.get(relay_pin)
+    if desc:
+        print(f"{get_elapsed():5.2f}: {desc} ON for {duration}s")
+    GPIO.output(relay_pin, GPIO.LOW)
+    if duration is not None:
+        await asyncio.sleep(duration, desc)
+        relay_off(relay_pin)
+
+
+def relay_off(relay_pin: int, desc: str | None = None):
+    if desc:
+        print(f"{get_elapsed():5.2f}: {desc} OFF")
+    GPIO.output(relay_pin, GPIO.HIGH)
+
+
+async def stagger_relay_starts():
+    task1 = asyncio.create_task(relay_on(RELAY_1, 2))
+    await asyncio.sleep(1)  # Stagger RELAY_2 by 1 second
+    task2 = asyncio.create_task(relay_on(RELAY_2, 2))
+    await asyncio.gather(task1, task2)
+
+
+def get_elapsed() -> float:
+    return time.time() - START_TIME
+
+
+async def main():
+    global START_TIME
+    START_TIME = time.time()
+    configure_relays()
+    for i in range(4):
+        await stagger_relay_starts()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
