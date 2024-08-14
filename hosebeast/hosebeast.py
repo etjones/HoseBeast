b716de275d75355990a5f0288ee9c2b9d4169db4
Evan Jones <evan_t_jones@mac.com>
2024-08-14 10:36:16 -0500

    - added red_green_button() for toggleable states - relay_x_on -> relay_x_off variable name plays slightly better with initial states. I got confused about states here and I'm not sure I'm thinking about this very clearly

--- hosebeast/hosebeast.py
+++ hosebeast/hosebeast.py
@@ -7,10 +7,10 @@ from .pages import index, about, profile, settings, table  # noqa: F401
 from .templates import template
 from . import styles
 
-# from . import relay_control
 from .relay_control import set_relay, RELAY_1, RELAY_2
 import reflex as rx
 
+from typing import Any
 
 # Create the app.
 app = rx.App(
@@ -24,18 +24,20 @@ app = rx.App(
 class HosebeastState(rx.State):
     """The app state."""
 
-    relay_1_on: bool = False
-    relay_2_on: bool = False
+    relay_1_off: bool = True
+    relay_2_off: bool = True
+
+    adc_gain: float = 1.0
+    adc_voltage: float = 2.5
+    adc_raw: int = 16000
 
     async def toggle_relay_1(self):
-        self.relay_1_on = not self.relay_1_on
-        # print(f"Relay 1 is now {self.relay_1_on}")
-        set_relay(RELAY_1, self.relay_1_on)
+        self.relay_1_off = not self.relay_1_off
+        set_relay(RELAY_1, self.relay_1_off)
 
     async def toggle_relay_2(self):
-        self.relay_2_on = not self.relay_2_on
-        # print(f"Relay 2 is now {self.relay_2_on}")
-        set_relay(RELAY_2, self.relay_2_on)
+        self.relay_2_off = not self.relay_2_off
+        set_relay(RELAY_2, self.relay_2_off)
 
 
 @template(route="/hosebeast", title="Hosebeast")
@@ -43,39 +45,35 @@ def hosebeast_layout() -> rx.Component:
     """The main layout of the app."""
     return rx.vstack(
         rx.heading("Hosebeast, Eh", size="2xl"),
-        rx.text(
-            "Get started by editing ",
-            rx.code("hosebeast/pages/index.py"),
-            ".",
+        red_green_button(
+            "Turn On Relay 1",
+            "Turn Off Relay 1",
+            var_conditional=HosebeastState.relay_1_off,
+            action=HosebeastState.toggle_relay_1,
         ),
-        rx.cond(
-            HosebeastState.relay_1_on,
-            rx.button(
-                "Turn off Relay 1",
-                on_click=HosebeastState.toggle_relay_1,
-                color_scheme="red",
-                width="100%",
-            ),
-            rx.button(
-                "Turn on Relay 1",
-                on_click=HosebeastState.toggle_relay_1,
-                color_scheme="green",
-                width="100%",
-            ),
+        red_green_button(
+            "Turn On Relay 2",
+            "Turn Off Relay 2",
+            var_conditional=HosebeastState.relay_2_off,
+            action=HosebeastState.toggle_relay_2,
         ),
-        rx.cond(
-            HosebeastState.relay_2_on,
-            rx.button(
-                "Turn off Relay 2",
-                on_click=HosebeastState.toggle_relay_2,
-                color_scheme="red",
-                width="100%",
-            ),
-            rx.button(
-                "Turn on Relay 2",
-                on_click=HosebeastState.toggle_relay_2,
-                color_scheme="green",
-                width="100%",
-            ),
-        ),
+    )
+
+
+def red_green_button(
+    true_label: str,
+    false_label: str,
+    var_conditional: Any,
+    action: Any,
+    false_action: Any | None = None,
+    **props,
+) -> rx.cond:
+    false_action = false_action or action
+    # Return a green and shows one label when var_conditional is True,
+    # or red and shows false_label otherwise.
+    # If false_action is not specified, call action for both conditions
+    return rx.cond(
+        var_conditional,
+        rx.button(true_label, on_click=action, color_scheme="green", **props),
+        rx.button(false_label, on_click=false_action, color_scheme="red", **props),
     )
